b1ecc1da809bd30965d20a7692b5449a83338dd5
Juan C. Roldán <juancarlos@sevilla.es>
2021-03-03 13:32:21 +0100

    Added all methods as first-level import to make it __simpler__.

---
+++ simpler/__init__.py
@@ -0,0 +1,12 @@
+from simpler.algorithms import DynamicProgramming
+from simpler.bioinformatics import codon_table, monoisotopic_mass_table, monoisotopic_mass_water, parse_fasta, dna_to_rna, rna_to_dna, rna_to_protein, reverse_complement
+from simpler.connectors import MySQL, Excel
+from simpler.files import cwd, read, write, disk_cache, size, find_hidden_compressed, tvshow_rename, directory_compare, safe_filename
+from simpler.format import human_bytes, human_seconds, human_date, random_string, print_matrix, safe_filename
+from simpler.mail import compose, send
+from simpler.math import clamp, snap, unique, all_equal, jaccard, levenshtein, base_change, prime_list, is_prime, fibonacci, lcm, gcd, factor, palindrome_list, phi, date_range
+from simpler.profiling import tic, toc, deep_size
+from simpler.sparql import dbpedia, entity_types
+from simpler.terminal import getch
+from simpler.validation import assert_set, assert_str, assert_number, assert_id, assert_mail, assert_exists
+from simpler.web import download_file, DownloaderPool
