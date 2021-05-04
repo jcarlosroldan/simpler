1e16bab3cab0a27d466092c4e0b478c53fd55140
Juan C. Roldán <juancarlos@sevilla.es>
2021-05-05 01:24:16 +0200

    Next theme test

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,9 @@ author = 'Juan C. Roldán'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode'
+	'sphinx.ext.autodoc',
+	'sphinx.ext.viewcode',
+	'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -46,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'master'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
