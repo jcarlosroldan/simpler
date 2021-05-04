4f4f3629a431a1368fe8711ce1626f802dc71c41
Juan C. Roldán <juancarlos@sevilla.es>
2021-05-05 01:22:41 +0200

    FIX: RTD blue theme

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,6 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -30,8 +29,7 @@ author = 'Juan C. Roldán'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-	'sphinx_rtd_theme'
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'master'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
