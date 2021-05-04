8503677c1665201f391a766acc9e3056b7edfdc2
Juan C. Roldán <juancarlos@sevilla.es>
2021-05-04 10:11:59 +0200

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r', encoding='utf-8') as f:
 
 setup(
 	name='simpler',
-	version='2.0.5',
+	version='2.0.6',
 	author='Juan C. Roldán',
 	author_email='juancarlos@sevilla.es',
 	description='Makes Python simpler.',
