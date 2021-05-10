eeccd9df88b244554372fcdf8d8ac09067d95498
Juan C. Roldán <juancarlos@sevilla.es>
2021-05-10 15:50:26 +0200

    FIX: error when raising errors on import_from_path.

--- simpler/reflection.py
+++ simpler/reflection.py
@@ -7,5 +7,5 @@ def import_from_path(path: str, name: str, module_name: str = '.') -> object:
 	spec = spec_from_file_location(module_name, path)
 	module = module_from_spec(spec)
 	spec.loader.exec_module(module)
-	assert name in module.__dict__, 'Name "%s" in path "%s" does not exist.' % (method, path)
+	assert name in module.__dict__, 'Name "%s" in path "%s" does not exist.' % (name, path)
 	return module.__dict__[name]
