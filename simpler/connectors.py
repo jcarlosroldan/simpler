4be23a8e598b2049ea967f2d3afe800854e46815
Juan C. Roldán <juancarlos@sevilla.es>
2021-06-16 14:56:26 +0200

    Non-backwards-compatible MySQL connector rewrite

--- simpler/connectors.py
+++ simpler/connectors.py
@@ -1,64 +1,166 @@
-from typing import Union
+from typing import Union, Any, Generator, List, Optional
+from MySQLdb import Connection
 from pandas import ExcelFile, read_excel, DataFrame, concat
 from numpy import arange, concatenate, array
 from re import compile, IGNORECASE
 
 class MySQL:
-	client = None
-	cursor = None
-
-	def __init__(self, autocommit: bool = True, *args, **kwargs) -> None:
-		''' Creates a MySQL database adapter with a handful of recurrent functions. '''
-		try:
-			from MySQLdb import Connection
-		except:
-			raise RuntimeError('Install MySQL or MariaDB and then do "pip install mysqlclient" to use this connector.')
-		self.connection = Connection(*args, **kwargs)
-		self.connection.autocommit(autocommit)
-		self.cursor = self.connection.cursor()
+	''' Connector for the MySQLdb backend with a handful of helpers. '''
+	def __init__(
+		self, host: str, user: str, password: str = None, db: str = None, charset: str = 'utf8mb4',
+		use_unicode: bool = True, autocommit: bool = True, max_insertions: int = None
+	) -> None:
+		self.max_insertions, self._cursor = max_insertions, None
+		self._connection = {
+			'host': host,
+			'user': user,
+			'charset': charset,
+			'use_unicode': use_unicode,
+			'autocommit': autocommit
+		}
+		if password:
+			self._connection.update({'passwd': password, 'auth_plugin': 'mysql_native_password'})
+		if db:
+			self._connection['db'] = db
+	
+	def __del__(self):
+		self.close()
 
 	def close(self) -> None:
 		''' Closes the current cursor and connection. '''
-		self.cursor.close()
-		self.connection.close()
+		if self._cursor is not None:
+			self._cursor.close()
+			self._connection.close()
 
-	def find_many(self, query: str, *params: tuple) -> list:
-		''' Returns a list of {column_name: value} dicts of the selected rows. '''
-		assert 'select' in query.lower()
-		self.cursor.execute(query, params)
-		return [
-			{k[0]: v for k, v in zip(self.cursor.description, row)}
-			for row in self.cursor.fetchall()
-		]
+	def get_cursor(self):
+		''' Returns the open cursor and initializes the connection if required. '''
+		if self._cursor is None:
+			self._connection = Connection(**self._connection)
+			self._cursor = self._connection.cursor()
+		return self._cursor
+	
+	def execute(self, query: str, params: tuple = None):
+		''' Wrapper for the MySQLdb execute method that won't send the params argument
+		if the params are empty, thus avoiding the need to replace % with %%. '''
+		return self.get_cursor().execute(query, params if len(params) else None)
 
-	def find_one(self, query: str, *params: tuple) -> dict:
-		''' Returns a {column_name: value} dict of the first selected row. '''
-		assert 'select' in query.lower()
-		self.cursor.execute(query, params)
-		row = self.cursor.fetchone()
-		if row is not None:
-			row = {k[0]: v for k, v in zip(self.cursor.description, row)}
-		return row
+	def find(self, query: str, *params: tuple) -> dict:
+		''' Returns a {column: value} dict of the first selected row. '''
+		row = self.find_one_tuple(query, *params)
+		if row:
+			return {k[0]: v for k, v in zip(self.get_cursor().description, row)}
+	
+	def find_tuple(self, query: str, *params: tuple) -> tuple:
+		''' Returns a tuple of the values of the first selected row. '''
+		self.execute(query, params)
+		return self.get_cursor().fetchone()
 
-	def find_column(self, query: str, *params: tuple) -> str:
-		''' Returns the value of the first attribute of a selected row. '''
-		assert 'select' in query.lower()
-		self.cursor.execute(query, params)
-		res = self.cursor.fetchone()
+	def find_all(self, query: str, *params: tuple) -> List[dict]:
+		''' Returns a list of {column: value} dicts of the selected rows. '''
+		return list(self.iter_all(query, *params))
+
+	def find_all_tuples(self, query: str, *params: tuple) -> List[tuple]:
+		''' Returns a list of tuples of the selected rows. '''
+		return list(self.iter_all_tuples(query, *params))
+
+	def iter_all(self, query: str, *params: tuple) -> Generator[dict]:
+		''' Returns a generator of {column: value} dicts of the selected rows. '''
+		rows = self.iter_many_tuples(query, *params)
+		description = self.get_cursor().description
+		for row in rows:
+			yield {k[0]: v for k, v in zip(description, row)}
+
+	def iter_all_tuples(self, query: str, *params: tuple) -> Generator[tuple]:
+		''' Returns a generator of tuples of the selected rows. '''
+		self.execute(query, params)
+		return self.get_cursor().fetchall()
+
+	def find_value(self, query: str, *params: tuple) -> Any:
+		''' Returns the value of the first column of the first selected row. '''
+		self.execute(query, params)
+		res = self.get_cursor().fetchone()
 		if res:
 			return res[0]
 
+	def find_column(self, query: str, *params: tuple) -> list:
+		''' Returns the value of the first column of every selected row. '''
+		return list(self.iter_column(query, *params))
+
+	def iter_column(self, query: str, *params: tuple) -> Generator[list]:
+		''' Returns a generator of the first column of every selected row. '''
+		self.execute(query, params)
+		for row in self.get_cursor().fetchall():
+			yield row[0]
+
 	def insert(self, query: str, *params: tuple) -> int:
-		''' Inserts a row and returns the id of the last one inserted. '''
-		assert 'insert' in query.lower()
-		self.cursor.execute(query, params)
-		return int(self.cursor.lastrowid)
+		''' Inserts a row and returns its id. '''
+		self.execute(query, params)
+		return int(self.get_cursor().lastrowid)
+
+	def insert_all(self, table: str, rows: list, tuple_rows: bool = True) -> Optional[int]:
+		''' Insert a list of rows and returns the id of the last one. By default,
+		these rows are a list of {column: value} dicts, but they can be inserted
+		from tuples of values setting `tuple_rows` to True. '''
+		if not len(rows): return
+		while self.max_insertions is not None and self.max_insertions < len(rows):
+			part, rows = rows[:self.max_insertions], rows[self.max_insertions:]
+			self.insert_many(table, part, tuple_rows)
+		if tuple_rows:
+			query = 'INSERT INTO %s VALUES %s' % (
+				table,
+				','.join(['(' + ','.join(['%s'] * len(rows[0])) + ')'] * len(rows))
+			)
+			params = [param for row in rows for param in row]
+		else:
+			keys = list(rows[0].keys())
+			values = '(%s)' % ','.join('%s' for _ in keys)
+			query = 'INSERT INTO %s(%s) VALUES %s' % (
+				table,
+				','.join(keys),
+				','.join(values for _ in rows)
+			)
+			params = [insertion[key] for insertion in rows for key in keys]
+		self.execute(query, params)
+		return int(self.get_cursor().lastrowid)
 
 	def apply(self, query: str, *params: tuple) -> int:
-		''' Executes an update or delete operation and returns the number of rows affected. '''
-		assert 'update' in query.lower() or 'delete' in query.lower()
-		self.cursor.execute(query, params)
-		return int(self.cursor.rowcount)
+		''' Applies a modification (update or delete) and returns the number of affected rows. '''
+		self.execute(query, params)
+		return int(self.get_cursor().rowcount)
+
+	def update(self, table: str, updates: dict = lambda: {}, filters: dict = lambda: {}) -> int:
+		''' Executes an update operation and returns the number of affected rows, specifying
+		a {column: value} list of updates and a filters list, i.e. `{'a': 4, 'b': None}` will be
+		translated into `WHERE A = 4 and B = NULL`. '''
+		query = 'UPDATE %s ' % table
+		params = []
+		if len(updates):
+			values = []
+			for k, v in updates.items():
+				values.append(k + '=%s')
+				params.append(v)
+			query += 'SET ' + ','.join(values)
+		if len(filters):
+			values = []
+			for k, v in filters.items():
+				values.append(k + '=%s')
+				params.append(v)
+			query += 'WHERE ' + ' AND '.join(values)
+		self.execute(query, params)
+		return int(self.get_cursor().rowcount)
+
+	def escape(self, value: Any, is_literal: bool = True) -> str:
+		''' Escapes the given value for its injection into the SQL query. By default,
+		the data `is_literal=True`, which will wrap strings with quotes for its insertion. '''	
+		if value is None:
+			value = 'NULL'
+		elif type(value) == str:
+			value = self._connection.escape_string(value).decode()
+			if is_literal:
+				value = '"%s"' % value
+		else:
+			value = self._connection.string_literal(value).decode()
+		return value
 
 class Excel:
 	''' Pandas Excel backend. '''
