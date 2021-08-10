4e5d0933ce4059167a72dd651574239dcedd04e4
Juan C. Roldán <juancarlos@sevilla.es>
2021-08-10 16:51:18 +0200

    Now `import_from_path` returns None when not found instead of raising an error.

--- simpler/reflection.py
+++ simpler/reflection.py
@@ -3,9 +3,9 @@ from os.path import exists
 
 def import_from_path(path: str, name: str, module_name: str = '.') -> object:
 	''' Loads the script at the specified path and returns an object given its name. '''
-	assert exists(path), 'Path "%s" is not valid' % path
-	spec = spec_from_file_location(module_name, path)
-	module = module_from_spec(spec)
-	spec.loader.exec_module(module)
-	assert name in module.__dict__, 'Name "%s" in path "%s" does not exist.' % (name, path)
-	return module.__dict__[name]
+	if exists(path):
+		spec = spec_from_file_location(module_name, path)
+		module = module_from_spec(spec)
+		spec.loader.exec_module(module)
+		if name in module.__dict__:
+			return module.__dict__[name]
