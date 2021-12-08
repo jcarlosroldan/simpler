c81318abb5d2b030f6b50c90cbac794a415d8af1
Juan C. Roldán <juancarlos@sevilla.es>
2021-12-08 14:27:47 +0100

    Missing expected condition import

--- simpler/web.py
+++ simpler/web.py
@@ -3,6 +3,7 @@ from requests import get
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
